67d63e97f31d975edce5ca7a53dd5641f017e50b
PhilNewm <Philippkd8@gmail.com>
2025-09-26 18:14:23 +0200

    functional implementation

--- files/api_download.py
+++ files/api_download.py
@@ -1,10 +1,15 @@
-import pprint
 import click
 import hashlib
+import json
 import os
 import requests
 import shutil
 import sidefx
+
+
+ACCESS_TOKEN_URL = "https://www.sidefx.com/oauth2/application_token"
+ENDPOINT_URL = "https://www.sidefx.com/api/"
+PRODUCTS_TO_INSTALL = "HOUDINI-NC;RENDER-NC"
 
 
 @click.group()
@@ -23,16 +28,13 @@ def get_package_name(
     production_only: bool,
     ) -> None:
 
-    # This service object retrieve a token using your Application ID and secret
     service = sidefx.service(
-        access_token_url="https://www.sidefx.com/oauth2/application_token",
+        access_token_url=ACCESS_TOKEN_URL,
         client_id=client_id,
         client_secret_key=client_secret_key,
-        endpoint_url="https://www.sidefx.com/api/",
+        endpoint_url=ENDPOINT_URL,
     )
 
-    # Retrieve the daily builds list, if you want the latest production
-    # you can skip this step
     releases_list = service.download.get_daily_builds_list(
         product='houdini',
         version=version,
@@ -62,24 +64,13 @@ def get_setup(
     package_dir: str
     ) -> None:
 
-    # This service object retrieve a token using your Application ID and secret
     service = sidefx.service(
-        access_token_url="https://www.sidefx.com/oauth2/application_token",
+        access_token_url=ACCESS_TOKEN_URL,
         client_id=client_id,
         client_secret_key=client_secret_key,
-        endpoint_url="https://www.sidefx.com/api/",
+        endpoint_url=ENDPOINT_URL,
     )
 
-    # Retrieve the daily builds list, if you want the latest production
-    # you can skip this step
-    # releases_list = service.download.get_daily_builds_list(
-    #     product='houdini',
-    #     version=version,
-    #     platform='linux',
-    #     only_production=production_only,
-    #     )
-
-    # Retrieve the latest production build available
     latest_release = service.download.get_daily_build_download(
         product='houdini',
         version=version,
@@ -87,7 +78,6 @@ def get_setup(
         platform='linux',
         )
 
-    # Download the file
     local_filename = latest_release['filename']
     result = requests.get(latest_release['download_url'], stream=True)
     downloads_dir = package_dir
@@ -111,6 +101,37 @@ def get_setup(
         raise Exception('Checksum does not match!')
 
     click.echo(download_path)
+
+
+@cli.command()
+@click.argument("client_id", type=click.STRING)
+@click.argument("client_secret_key", type=click.STRING)
+@click.argument("server_name", type=click.STRING)
+@click.argument("server_code", type=click.STRING)
+@click.argument("version", type=click.STRING)
+def get_non_commercial_license_key(
+        client_id: str,
+        client_secret_key: str,
+        server_name: str,
+        server_code: str,
+        version: str
+    ) -> None:
+
+    service = sidefx.service(
+        access_token_url=ACCESS_TOKEN_URL,
+        client_id=client_id,
+        client_secret_key=client_secret_key,
+        endpoint_url=ENDPOINT_URL,
+    )
+
+    license_strings = service.license.get_non_commercial_license(
+        server_name=server_name,
+        server_code=server_code,
+        version=version,
+        products=PRODUCTS_TO_INSTALL,
+    )
+
+    click.echo(json.dumps(license_strings["license_keys"], indent=2))
 
 if __name__ == "__main__":
     cli()
